329642e9d7b9329f9e4197c3a830c53d444acd95
Jelle Kootwijk <kootwijkjelle@gmail.com>
2021-01-14 15:30:49 +0100

    Added Settings page
    - added settings page with general info

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flaskext.mysql import MySQL
 
+import socket, math
+import psutil
 
 app = Flask(__name__)
 
@@ -14,6 +16,15 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
+def convert_size(size_bytes):
+  if size_bytes == 0:
+      return "0B"
+  size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+  i = int(math.floor(math.log(size_bytes, 1024)))
+  p = math.pow(1024, i)
+  s = round(size_bytes / p, 2)
+  
+  return "%s %s" % (s, size_name[i])
 
 @app.route("/")
 def home():
@@ -34,6 +45,17 @@ def database():
   data = cursor.fetchall()
   
   return render_template('database.html', data=data)
+
+@app.route("/settings")
+def settings():
+  ipaddress = socket.gethostbyname(socket.gethostname())
+  
+  network_stats = psutil.net_io_counters(pernic=True)['Ethernet']
+  bytes_sent = getattr(network_stats, 'bytes_sent')
+  bytes_recv = getattr(network_stats, 'bytes_recv')
+  
+  
+  return render_template('settings.html', ip=ipaddress, byte_sent=convert_size(bytes_sent), byte_recv=convert_size(bytes_recv))
 
 if __name__ == "__main__":
   app.run(debug=True)
